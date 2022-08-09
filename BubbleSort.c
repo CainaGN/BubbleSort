2b98605007053ee3a9cbf942cb1645f8c1843b6d
CainaGN <102558246+CainaGN@users.noreply.github.com>
2022-08-09 13:21:05 -0400

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,6 +1,9 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define size_v 1000
+#include <time.h>  
 /*
 quantidade de trocas
 quanti variáveis
@@ -24,7 +27,7 @@ void bubble(int *vet[], int size)
     while ( n > 0)
     {
    
-    for(int i = 0; i < 8; i++){
+    for(int i = 0; i < size_v; i++){
 
         if(vet[i] >= vet[i+1]){
            swap(&vet[i], &vet[i+1]);
@@ -39,28 +42,38 @@ void bubble(int *vet[], int size)
 
 int main(){
     
-    int vet[8];
+    int vet[size_v];
     int n = sizeof(vet);
-     
-       for (int i = 0; i < 8; i++)
+    double timer = 0.0;
+    time_t begin = time(NULL);
+
+
+
+       for (int i = 0; i < size_v; i++)
   {
     
     vet[i] = (rand() % 100);
   }
 
-        for (int i = 0; i < 8; i++)
+      /*  for (int i = 0; i < size_v; i++)
         {
             printf("%d ", vet[i]);
-        }
+        }*/
 
 
-    bubble(&vet, 8);
+    bubble(&vet, size_v);
 
     printf("\n");
-    for (int i = 0; i < 8; i++)
+    /*for (int i = 0; i < size_v; i++)
         {
             printf("%d ", vet[i]);
-        }
+        }*/
 
-
+        time_t end = time(NULL);
+        
+        timer = end - begin;
+        printf("\ntime: %f ms\n", timer);
+        
+        return 0;
 }
+
