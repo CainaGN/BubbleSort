b3849816c53783b27e0c87ba7fd0177eab161c86
CainaGN <102558246+CainaGN@users.noreply.github.com>
2022-08-17 22:50:03 -0400

    Update tad.c

--- tad.c
+++ tad.c
@@ -1,10 +1,16 @@
-#include "float_vector.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "float_vector.h"
 
-int main(int agc, char const *argv[]){
-    FloatVector *vetor = create(10);
-
-    return 0;
+int main(int argc, char const *argv[])
+{
+  int tam = argv[1];
+  /* code */
+  FloatVector *vetor = create(2);
+  append(&vetor, 0.0);
+  append(&vetor, 1.0);
+  
+  print(&vetor);
+  return 0;
 }
