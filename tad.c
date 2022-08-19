42e37475d7e2e25c432ffa6432334788a4e76419
CainaGN <102558246+CainaGN@users.noreply.github.com>
2022-08-18 23:44:27 -0400

    Update tad.c

--- tad.c
+++ tad.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "float_vector.h"
+//#include "float_vector.h"
+#include "float_vector.c"
+#define tam_v 7
 
 int main(int argc, char const *argv[])
 {
-  int tam = argv[1];
+  int tam = (int)argv[1];
   /* code */
-  FloatVector *vetor = create(2);
-  append(&vetor, 0.0);
-  append(&vetor, 1.0);
+  FloatVector *vetor = create(tam_v);
   
-  print(&vetor);
+  
+  /*append(vetor, 7.0);
+  append(vetor, 1.0);
+  append(vetor, 6.0);
+  append(vetor, 3.0);
+  append(vetor, 4.0);
+  append(vetor, 5.0);
+  append(vetor, 8.0);*/
+ for(int i = 0; i < tam_v; i++)
+ {
+  float value = (float)rand()/(float)(RAND_MAX/100);
+  append(vetor, value);
+ }
+  
+  print(vetor);
+  printf("\n\n");
+ 
+  bubble(vetor, 7); 
+
+
+  print(vetor);
   return 0;
 }
