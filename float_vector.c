179c6624773aa551b0d8122e1cdd4ff258acf5cc
CainaGN <102558246+CainaGN@users.noreply.github.com>
2022-08-19 20:36:17 -0400

    Update float_vector.c

--- float_vector.c
+++ float_vector.c
@@ -69,9 +69,15 @@ void print(const FloatVector *vector)
         printf("[%d] = %f\n", i, vector->data[i]);
     }
 
+   /* FloatVector *p;
+        for(p = vector; p!= NULL; p = p->next){
+            printf("%f\n", p->data);
+        }*/
+
 }
 
-void append(FloatVector *vector, float n) {
+void append(FloatVector *vector, float n) 
+{
    
    // printf("teste 1 concluido\n");
     vector->data[vector->size] = n;
@@ -81,7 +87,8 @@ void append(FloatVector *vector, float n) {
 
 }
 
-void swap(float *a, float *b){
+void swap(float *a, float *b)
+{
 
             float aux = *b;
             *b = *a;
@@ -90,31 +97,153 @@ void swap(float *a, float *b){
 }
 
 
-void bubble(float *vetor[], int tam_v)
+void bubble(const FloatVector *vetor, int tam_v)
 {
+  
+  //printf("%f\t", vetor->data[3]);
+  
+   //printf("Teste 1 completo\n");
    unsigned long int trocas = 0, comparacoes = 0;
-
+   
     for( int n = 0; n < tam_v - 1; n++)
     {
        bool swapped = false;
-    
-    for(int i = 0; i < tam_v - 1; i++)
+       //printf("Teste 2 completo\n");
+       for(int i = 0; i < tam_v - 1; i++)
         {
-
-            if(vetor[i] >= vetor[i+1])
+            if(vetor->data[i] >= vetor->data[i+1])
             {
-            swap(&vetor[i], &vetor[i+1]);
+            //printf("%f\t", vetor->data[i]);
+            swap(&vetor->data[i], &vetor->data[i+1]);
             swapped = true;
             trocas++;
+            //printf("Teste 4 completo\n");
             }
             comparacoes++;
         }
         if(swapped == false)
             {
+                //printf("BREAK\n");
                 break;
             }
-    
+    //printf("Teste 5 completo\n");
         
     }
+       // print(&vetor);
+}   
+
+
+void selectionSort(const FloatVector *vetor, int tam_v)
+{
+   //unsigned long int trocas = 0, comparacoes = 0;
+   //FILE *file;
+   //file = fopen("dados.txt", "a");
+    for(int i = 0; i < tam_v - 1; i++)
+    {
+        int menor  = i; 
+        for(int j = i + 1; j < tam_v; j++)
+        {
+            if(vetor->data[j] < vetor->data[menor])
+            {
+                menor = j;
+               // comparacoes++;
+            }
+            
+            if(menor != i){
+            swap(&vetor->data[i], &vetor->data[menor]);
+           // trocas++;
+            //comparacoes++;
+            }
+        }
+
+    }
+   //fprintf(file, "\nTrocas: %d\nComparacoes: %d\nTamanho do vetor: %i\n\n", trocas, comparacoes, size_v);
+}
+
+
+void insertionSort(const FloatVector *vetor, int tam_v)
+{
+   //unsigned long int Movimentacao = 0;
+   //FILE *file;
+   //file = fopen("dados.txt", "a");
+    
+    for (int i = 1; i < tam_v; i++) 
+    {
         
+        int j = i - 1;
+        float n = vetor->data[i];
+        
+        while (j >= 0 && vetor->data[j] > n) {
+            vetor->data[j + 1] = vetor->data[j];
+            j = j - 1;
+     //       Movimentacao++;
+        }
+        vetor->data[j + 1] = n;
+    }
+   //fprintf(file, "\nMovimentos: %d\nTamanho do vetor: %i\n\n", Movimentacao, size_v);
+
+}
+
+
+
+
+void mergesort(const FloatVector *vetor, int inicio, int fim)
+{
+
+    if(inicio < fim)
+    {
+        int meio = inicio + (fim - inicio) / 2;
+        mergesort(vetor, inicio, meio);
+        mergesort(vetor, meio + 1, fim); 
+        merge(vetor, inicio, meio, fim);
+    }
+
+}
+
+void merge(const FloatVector *vetor, int inicio, int meio, int fim)
+{
+    int n1 = meio - inicio + 1;
+    int n2 = fim - meio;
+
+    float L[n1];
+    float R[n2];
+
+    for (int i = 0; i < n1; i++)
+    {
+        L[i] = vetor->data[inicio + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+      R[i] = vetor->data[meio + 1 + i];
+    }
+    
+    int i = 0, j = 0, k = inicio;
+    
+    while(i < n1 && j < n2)
+    {
+        if(L[i] <= R[j])
+        {
+            vetor->data[k] = L[i];
+            i++;
+        } else {
+                    vetor->data[k] = R[j];
+                    j++;
+               }
+        k++;
+
+    }
+
+    while( j < n2)
+    {
+        vetor->data[k] = R[j];
+        j++;
+        k++;
+    }
+    while( i < n1)
+    {
+        vetor->data[k] = L[i];
+        i++;
+        k++;
+    }
+
 }   
