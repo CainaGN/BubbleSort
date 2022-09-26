4568ef0e09a7d742c7ff1301391ef641ef5efca2
CainaGN <102558246+CainaGN@users.noreply.github.com>
2022-09-26 14:51:24 -0400

    Update linked_list.h

--- LinkedList/linked_list.h
+++ LinkedList/linked_list.h
@@ -3,8 +3,14 @@
 
 typedef struct _snode SNode;
 typedef struct _slinked_list LinkedList;
-
+void print_LL(LinkedList *L);
 SNode *SNode_create(int val);
-
+void remove_node(SNode** head, int position);
+void search(LinkedList *L, int value);
+void add_last(LinkedList *L, int val);
+void add_first(LinkedList *L, int val);
+LinkedList *LinkedList_create();
+SNode *SNode_create(int val);
+void LinkedList_destroy(LinkedList **L_ref);
 
 #endif
