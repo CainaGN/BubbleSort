9949fa84953ae26ed7d25d93955ec7bdad74f084
CainaGN <102558246+CainaGN@users.noreply.github.com>
2022-10-01 20:04:28 -0400

    Update LL_Dupla.h

--- Lista Duplamente encadeada/LL_Dupla.h
+++ Lista Duplamente encadeada/LL_Dupla.h
@@ -7,7 +7,7 @@ List *List_create();
 
 void List_Destroy(DoubleLinkedList **list); //implementado em aula
 bool List_is_empty(const DoubleLinkedList *list);// implementado em aula
-void List_add_first(DoubleLinkedList *list, int val);
+void List_add_first(DoubleLinkedList *list, int val);  //(v)
 void List_add_last(DoubleLinkedList *list, int val);//implementar
 void List_print(const DoubleLinkedList *list);//implementar
 void List_inverted_print(const DoubleLinkedList *list); //implementar
